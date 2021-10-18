3cc329f952018e326815643099544dd0293525aa
ngustavin <gust4f3rn@gmail.com>
2021-10-18 19:53:49 +0400

    github auth route created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
+import 'dotenv/config'
 import express from 'express'
 
 const app = express()
+
+app.get('/github', (req, res) => {
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
 
 app.listen(4000, () => console.log('🚀 Server is running on port 4000'))
